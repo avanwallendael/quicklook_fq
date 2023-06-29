17b5cb0a7a83b496fe3b35795b89fe6d3704d906
Acer VanWallendael <acer@Acers-MacBook-Pro.local>
2023-06-29 15:48:01 -0400

    Added color option to quicklook_fq_plot.R

--- quicklook_fq_plot.R
+++ quicklook_fq_plot.R
@@ -21,10 +21,19 @@ mash_dist<-as.dist(mash_mat)
 
 #run pcoa####
 #alter k to get more pcs
-pcoa<-cmdscale(mash_dist,k = 3)
+pcoa<-as.data.frame(cmdscale(mash_dist,k = 3))
+
+#to color by population, import a file with the format shown below
+#pop_cols_format<-data.frame(filenames=rownames(pcoa), pop=c(rep("popA",5),rep("popB", nrow(pcoa)-5)))
+#pop_cols_format$col<-rainbow(length(unique(pop_cols_format$pop)))[match(pop_cols_format$pop, unique(pop_cols_format$pop))]
+#pcoa$col<-pop_cols_format$col[match(rownames(pcoa), pop_cols_format$filenames)]
 
 #plot pcs 1 and 2
 pdf("mash_out.pdf", height=7, width=7)
-plot(pcoa[,1], pcoa[,2])
-text(pcoa[,1], pcoa[,2], labels = rownames(pcoa))  
+plot(pcoa[,1], 
+     #col=pcoa$col,
+     pcoa[,2])
+text(pcoa[,1], pcoa[,2], 
+     #col=pcoa$col,
+     labels = rownames(pcoa))  
 dev.off()
